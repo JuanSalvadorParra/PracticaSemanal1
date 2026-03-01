c37215c254a6ccead028d0b832f4bed28b6fe33f
JuanSalvadorParra <juan.parra.rincon@est.una.ac.cr>
2026-03-01 15:03:01 -0600

    Tarea

--- Practica/PracticaSemanal1/main.cpp
+++ Practica/PracticaSemanal1/main.cpp
@@ -4,6 +4,8 @@ using namespace std;
 
 int main() {
 
+    cout << "Actividad 1: " << endl;
+
     Perro *perro = new Perro();
 
     Animal* animal = perro;
@@ -23,6 +25,19 @@ int main() {
     delete perro;
     delete mamifero;
     delete animal;
+
+    cout << "Fin actividad 1 " << endl;
+
+    cout << endl << "Actividad 2: " << endl;
+
+    Animal* animal1 = new Perro();
+    try {
+        Perro* perro2 = dynamic_cast<Perro*>(animal1);
+        perro2->sonido();
+    }
+        catch (runtime_error& error) {
+            cout << "Error de creacion el perro" << endl;
+        }
 
     return 0;
 }
